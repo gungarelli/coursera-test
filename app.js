f55cc3b38c8f4d2a358b7c2397e5b7a064b7278f
Gianni <gianernesto.ungarelli@telespazio.com>
2024-11-15 08:19:18 +0100

    Second assignment

--- app.js
+++ app.js
@@ -1,29 +1,80 @@
-(function () {
+// app.js
+(function() {
 'use strict';
 
-angular.module('LunchCheck', [])
-.controller('LunchCheckController', LunchCheckController);
+angular.module('ShoppingListCheckOff', [])
+  .controller('ToBuyController', ToBuyController)
+  .controller('AlreadyBoughtController', AlreadyBoughtController)
+  .service('SharedService', SharedService);
 
-  LunchCheckController.$inject = ['$scope'];
 
-  function LunchCheckController($scope) {
-     $scope.lunchItems = '';
-     $scope.message = '';
+  function SharedService() {
+    var service = this;
+    var toBuyItems  = [
+      { name: "cookies", quantity: 10 },
+      { name: "milk", quantity: 2 },
+      { name: "bread", quantity: 1 },
+      { name: "eggs", quantity: 12 },
+      { name: "apples", quantity: 5 } ];
+    var alreadyBoughtItems = [];
 
-     $scope.checkLunch = function() {
-          if (!$scope.lunchItems.trim()) {
-                $scope.message = "Please enter data first";
-                return;
-          }
+    service.getToBuyItems = function() {
+      return toBuyItems;
+    };
 
-          const items = $scope.lunchItems.split(',');
-          const itemCount = items.length;
+    service.getAlreadyBoughtItems = function() {
+      return alreadyBoughtItems;
+    };
 
-          if (itemCount <= 3) {
-              $scope.message = "Enjoy!";
-          } else {
-              $scope.message = "Too much!";
-          }
-      };
+    service.transferItem = function(index) {
+      var item = toBuyItems.splice(index, 1)[0];
+      alreadyBoughtItems.push(item);
+    };
+    service.addToBuyItem = function(name, quantity) {
+       var item = { name: name, quantity: quantity };
+       toBuyItems.push(item);
+     };
+
+     service.removeToBuy = function(index) {
+        toBuyItems.splice(index,1);
+     };
+     service.removeBought = function(index) {
+        alreadyBoughtItems.splice(index,1);
+     };
+  }
+
+// toBuyList #1 - controller
+ToBuyController.$inject = ['SharedService'];
+function ToBuyController(SharedService) {
+  var toBuyList = this;
+  toBuyList.items = SharedService.getToBuyItems();
+  toBuyList.itemName = "";
+  toBuyList.itemQuantity = "";
+
+  toBuyList.addItem = function() {
+    SharedService.addToBuyItem(toBuyList.itemName,
+      toBuyList.itemQuantity)
+    toBuyList.itemName = "";
+    toBuyList.itemQuantity = "";
+  };
+
+  toBuyList.markAsBought = function(index) {
+    SharedService.transferItem(index);
+  };
+ // remove item at index from toBuyList
+  toBuyList.removeItem = function(index) {
+    SharedService.removeToBuy(index);
+  };
+}
+
+// boughtList #2 - controller
+AlreadyBoughtController.$inject = ['SharedService' ];
+function AlreadyBoughtController(SharedService) {
+    var boughtList = this;
+    boughtList.items = SharedService.getAlreadyBoughtItems();
+    // remove item at index from boughtList
+    boughtList.removeItem = function(index) {
+      SharedService.removeBought(index);
+    };
   }
 })();
